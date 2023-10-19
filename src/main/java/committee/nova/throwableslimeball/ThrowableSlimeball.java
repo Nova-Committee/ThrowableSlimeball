24947bc6c8324ffe09979c1ed1bbde6fd159bf37
MikhailTapio <mikhailtapio13468@163.com>
2023-10-19 20:09:38 +0800

    Add dispenser behavior, balanced

--- src/main/java/committee/nova/throwableslimeball/ThrowableSlimeball.java
+++ src/main/java/committee/nova/throwableslimeball/ThrowableSlimeball.java
@@ -1,15 +1,25 @@
 package committee.nova.throwableslimeball;
 
 import committee.nova.throwableslimeball.common.config.CommonConfig;
+import committee.nova.throwableslimeball.common.entity.impl.MagmaCream;
+import committee.nova.throwableslimeball.common.entity.impl.Slimeball;
 import committee.nova.throwableslimeball.common.entity.init.EntityTypeReference;
 import committee.nova.throwableslimeball.common.item.init.ItemReference;
+import net.minecraft.Util;
+import net.minecraft.core.Position;
+import net.minecraft.core.dispenser.AbstractProjectileDispenseBehavior;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.DispenserBlock;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
@@ -36,5 +46,17 @@ public class ThrowableSlimeball {
         EntityTypeReference.init();
         ITEMS_VANILLA.register(bus);
         ENTITIES.register(bus);
+        DispenserBlock.registerBehavior(Items.SLIME_BALL, new AbstractProjectileDispenseBehavior() {
+            @Override
+            protected Projectile getProjectile(Level level, Position position, ItemStack itemStack) {
+                return Util.make(new Slimeball(level, position.x(), position.y(), position.z()), s -> s.setItem(itemStack));
+            }
+        });
+        DispenserBlock.registerBehavior(Items.MAGMA_CREAM, new AbstractProjectileDispenseBehavior() {
+            @Override
+            protected Projectile getProjectile(Level level, Position position, ItemStack itemStack) {
+                return Util.make(new MagmaCream(level, position.x(), position.y(), position.z()), s -> s.setItem(itemStack));
+            }
+        });
     }
 }
